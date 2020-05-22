a005c32233691189947bbcef8769a73e982023af
Jesse Noller <jnoller@gmail.com>
2020-05-22 12:53:32 -0600

    setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,46 @@
 #!/usr/bin/env bash
+
+sudo apt-get update && apt-get upgrade -y
+
+apt-get install -y git \
+    build-essential \
+    linux-headers-"$(uname -r)" \
+    htop \
+    iotop \
+    vim \
+    libpq-dev \
+    libssl-dev \
+    openssl \
+    libffi-dev \
+    zlib1g-dev \
+    libaio-dev \
+    sysstat
+
+
+# Add the attached disks
+disks=$(fdisk -l | grep Disk | grep "/dev" | awk '{print $2}' | cut -d ":" -f1)
+for i in ${disks};
+do
+    if [ "${i}" = "/dev/sda" ]; then
+        mkdir -p /os-disk
+    elif [ "${i}" = "/dev/sdb" ]; then
+        ln -fs /mnt /ephemeral
+    else
+        echo "nah"
+        sizen=$(fdisk "${i}" -l | grep Disk | grep "/dev" | awk '{print $3$4}' | cut -d "," -f1)
+        echo "${i} :: ${sizen}"
+        mkdir -p "/${sizen}" || exit 1
+        parted "${i}" --script mklabel msdos mkpart primary ext4 0% 100% || exit 1
+        # Leave mkfs defaults, permutations to test:
+        # -b block-size [512 | 1024 | 2048 | 4096]
+        # [-i bytes-per-inode] [-I inode-size]
+        mkfs.ext4 -F "${i}"  # TBD: -o agblksize={ 512 | 1024 | 2048 | 4096 } || exit 1
+        echo "${i}    /${sizen} ext4  defaults,noatime        0 0" >>/etc/fstab
+        mount -o remount "${i}"
+    fi
+done
+
+( cd /
+    ln -s /mnt /ephemeral
+    mkdir -p /os-disk
+)
